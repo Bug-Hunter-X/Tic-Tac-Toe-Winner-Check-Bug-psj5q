8feba44b332a6ebc1c0bbadebe9d8b4d374ebda5
BugHunter-X <askzenai@gmail.com>
2024-12-29 12:38:07 +0100

    Add checkWinnerBug.js file

---
+++ checkWinnerBug.js
@@ -0,0 +1,25 @@
+function checkWinner(board) {
+  // Check rows
+  for (let i = 0; i < 3; i++) {
+    if (board[i][0] !== '' && board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+      return board[i][0];
+    }
+  }
+
+  // Check columns
+  for (let i = 0; i < 3; i++) {
+    if (board[0][i] !== '' && board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+      return board[0][i];
+    }
+  }
+
+  // Check diagonals
+  if (board[0][0] !== '' && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+    return board[0][0];
+  }
+  if (board[0][2] !== '' && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+    return board[0][2];
+  }
+
+  return null; // No winner
+}
